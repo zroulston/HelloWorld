be5087ed723845ebff71a4f924e0f06e17664808
bradwilliamson <bradtech519@live.com>
2016-12-18 20:14:30 -0600

    Added powershell hello world

---
+++ HelloWorld.ps1
@@ -0,0 +1,18 @@
+﻿#Brad Williamson
+#Hello World Class requires PowerShell 5.0
+#
+#
+#
+#
+Class HelloWorld {
+[String]$Name
+[String]$Description
+[String]$owner
+}
+
+$Bradsworld = [HelloWorld]::new()
+$Bradsworld.Name = "HelloWorld"
+$Bradsworld.Description = "Brad's Hello World"
+$Bradsworld.owner = 'Brad'
+
+Write-Host $Bradsworld
